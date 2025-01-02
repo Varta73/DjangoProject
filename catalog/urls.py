773884a9cace487c70b94662b564a1435f6a063d
Svetlana Bazhenova <varta.73@mail.ru>
2025-01-02 14:43:17 +0300

    Добавление нового функционала

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list, contacts
+from catalog.views import product_list, contacts, product_detail
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', product_list, name='product_list'),
-    path('contacts/', contacts, name='contacts')
+    path('contacts/', contacts, name='contacts'),
+    path('products/<int:pk>', product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -15,4 +15,11 @@ def product_list(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
+
 
